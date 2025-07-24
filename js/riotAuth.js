2c4e4c304327ac8fafa82413e68ca548a78b0010
SunnyWang0 <sunnywrocks@gmail.com>
2025-07-23 23:40:10 -0700

    license

--- js/riotAuth.js
+++ js/riotAuth.js
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2024 EloWard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * "Commons Clause" License Condition v1.0
+ * The Software is provided to you by the Licensor under the License, as defined below, 
+ * subject to the following condition. Without limiting other conditions in the License, 
+ * the grant of rights under the License will not include, and the License does not grant 
+ * to you, the right to Sell the Software.
+ */
+
 // EloWard Riot RSO Authentication
 import { PersistentStorage } from './persistentStorage.js';
 
